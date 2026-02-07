fa14ebd8c330d9f5b10f4868e5249f3b83c33995
amartello <lunafreelancerr@gmail.com>
2026-02-07 20:18:38 -0300

    modifco il index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,28 +4,27 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
 
-
-
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Conexión a Mongo Atlas
 connectDB();
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Rutas principales
 app.use("/api/reservations", reservationRoutes);
 
-
-// ruta de prueba
+// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("API de Reservas funcionando 🚀");
 });
 
-// levantar servidor
+// Levantar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
